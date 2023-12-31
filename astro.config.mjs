6617c086d84d1ed91e0fdd07b576c03fcdac539a
Frank <chenlong365@gmail.com>
2023-12-31 15:01:23 +0800

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lucent-daifuku-4f96b3.netlify.app',
+  integrations: [preact()]
 });
